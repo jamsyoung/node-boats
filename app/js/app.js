1343e64fa58c5b977d286d7a7c5e34b70b22accd
Matt Crutchfield <matt.crutchfield@turner.com>
2014-05-29 16:51:52 -0400

    more things

--- app/js/app.js
+++ app/js/app.js
@@ -1,34 +1,52 @@
 var boat = angular.module('boat', []);
 
-boat.controller('boatController',['$scope', '$http', function ($scope, $http){
+boat.controller('boatController',['$scope', '$http', '$timeout', function ($scope, $http, $timeout){
 
         function log (method, message) {
-            // $http.get('/log/add', message).success(function(){console.log('win')});
             console.log(method + ': ' + message);
         }
 
         function trigger (method) {
-            $http({ method: 'GET', url: '/api/v1/' + method + '/' + $scope[method] }).
-                success(function (data, status, headers, config) {
+            $http({ method: 'GET', url: '/api/v1/' + method + '/' + $scope[method] })
+                .success(function (data, status, headers, config) {
                     log(method, 'it worked');
-                }).
-                error(function (data, status, headers, config) {
+                })
+                .error(function (data, status, headers, config) {
                     log(method, 'it failed');
                 });
         }
+
+        var timeoutPromise,
+            timeoutPromise2;
 
         $scope.motorspeed = 0;
         $scope.angle = 90;
 
         $scope.togglePower = function () {
             $scope.motorspeed = 0
+            trigger('motorspeed');
         };
 
         $scope.$watch('motorspeed', function () {
-            trigger('motorspeed');
+
+            if (timeoutPromise) {
+                $timeout.cancel(timeoutPromise);
+            }
+
+            timeoutPromise = $timeout(function () {
+                trigger('motorspeed');
+            }, 500);
         });
 
         $scope.$watch('angle', function () {
-            trigger('angle');
+
+            if (timeoutPromise2) {
+                $timeout.cancel(timeoutPromise2);
+            }
+
+            timeoutPromise2 = $timeout(function () {
+                trigger('angle');
+            }, 500);
         });
+
 }]);
