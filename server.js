f1a52cb453e64a8a303bcc0283e008ba4c9d222c
James Young <jmeyoung@gmail.com>
2014-05-29 16:02:28 -0400

    error handling

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 'use strict';
 
 var
+    accessToken = '868ea91f13af5f3e18b4c69875a011155a2c82d8',
+    app,
     bodyParser = require('body-parser'),
     deviceId = '51ff6f065067545715550287',
     express = require('express'),
-    app = express(),
-    ejs = require('ejs'),
-    logger = require('./logger/lib/logger'),
+    logger = require('morgan'),
     path = require('path'),
     port = process.env.npm_package_config_port || 3000,
     request = require('request'),
-    sparkEndpoint = 'https://api.spark.io/v1/devices/',
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-    accessToken = '868ea91f13af5f3e18b4c69875a011155a2c82d8';
+    sparkEndpoint = 'https://api.spark.io/v1/devices/';
+
+app = express();
 
 
 /* middleware */
+app.use(logger());
 app.use(bodyParser());
 app.use(express['static'](__dirname + '/app'));
 app.use(express['static'](__dirname + '/logger/public'));
@@ -26,22 +26,12 @@ app.set('views', path.normalize(__dirname + '/logger/views'));
 app.engine('html', require('ejs').__express);
 
 
-/* logger */
-logger.init(io, true, 100);
-
 
 /* application routes */
 app.get(/^\/(index.html)?$/i, function (request, response) {
     response.sendfile('./app/views/index.html');
 });
 
-
-/* logging */
-app.get('/log', logger.view);
-app.get('/log/add', logger.add);
-app.get('/log/logmessage', logger.add);
-app.get('/log/clear', logger.clear);
-app.get('/log/test', logger.test);
 
 
 /* api routes */
@@ -55,33 +45,69 @@ app.get('/api/v1/test/:func/:value', function (req, res) {
 });
 
 app.get('/api/v1/motorspeed/:value', function (req, res) {
+    var sparkRequestUri = sparkEndpoint + deviceId + '/motorSpeed';
+
     if (req.params.value <= 255 && req.params.value >= -255) {
-        request.post(sparkEndpoint + deviceId + '/motorSpeed', function (error, response, body) {
-            res.json({func: 'motorSpeed', value: req.params.value, response: body});
-            console.log(body);
+        console.log('POST request to: %s with args: %s', sparkRequestUri, req.params.value);
+
+        request.post(sparkRequestUri, function (error, response, body) {
+            if (error) {
+                console.error('error: %j', error);
+                res.json(error);
+            }
+
+            console.log('response from %s: %s', sparkRequestUri, body);
+
+            res.json({
+                func: 'motorSpeed',
+                value: req.params.value,
+                response: JSON.parse(body)
+            });
         }).form({
             access_token: accessToken,
             args: req.params.value
         });
-
-//        res.json({func: 'motorSpeed', value: req.params.value});
     } else {
-        res.json({error: 'value must be between -255 and 255'});
+        console.error('value must be between -255 adn 255');
+
+        res.json({
+            error: 'value must be between -255 and 255'
+        });
     }
 });
 
 app.get('/api/v1/angle/:value', function (req, res) {
+    var sparkRequestUri = sparkEndpoint + deviceId + '/angle';
+
     if (req.params.value <= 175 && req.params.value >= 0) {
-        request.post(sparkEndpoint + deviceId + '/angle').form({
+        console.log('POST request to: %s with args: %s', sparkRequestUri, req.params.value);
+
+        request.post(sparkRequestUri, function (error, response, body) {
+            if (error) {
+                console.error('error: %j', error);
+                res.json(error);
+            }
+
+            console.log('response from %s: %s', sparkRequestUri, body);
+
+            res.json({
+                func: 'angle',
+                value: req.params.value,
+                response: JSON.parse(body)
+            });
+        }).form({
             access_token: accessToken,
             args: req.params.value
         });
-
-        res.json({func: 'angle', value: req.params.value});
     } else {
-        res.json({error: 'value must be between 0 and 175'});
+        console.error('value must be between 0 and 175');
+
+        res.json({
+            error: 'value must be between 0 and 175'
+        });
     }
 });
+
 
 /* start server */
 console.log('node boat listening on port %d, cap\'n', port);
