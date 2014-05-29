7d2097e8d773c54e7ad07d18d602a9e32d93299d
James Young <jmeyoung@gmail.com>
2014-05-29 14:43:16 -0400

    server work

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 'use strict';
 
 var
+    accessToken = '868ea91f13af5f3e18b4c69875a011155a2c82d8',
     app,
-    http = require('http'),
     bodyParser = require('body-parser'),
+    deviceId = '51ff6f065067545715550287',
     express = require('express'),
-    ejs = require('ejs'),
     logger = require('morgan'),
     path = require('path'),
+    port = process.env.npm_package_config_port || 3000,
     request = require('request'),
-    boat = require('./app.js'),
-    port = process.env.PORT || process.env.npm_config_port || process.env.npm_package_config_port || 3000;
+    sparkEndpoint = 'https://api.spark.io/v1/devices/';
 
 app = express();
+
 
 /* middleware */
 app.use(logger());
@@ -22,17 +23,34 @@ app.set('view engine', 'html');
 app.use(express['static'](path.join(__dirname, '/app')));
 app.set('views', path.join(__dirname, '/app/views'));
 
-/* application routes */
 
-/* main page */
+/* application routes */
 app.get('/', function (req, res) {
     res.render('index.html');
 });
+
+
+/* api routes */
+app.get('/api/v1/:func/:value', function (req, res) {
+
+    request.post(sparkEndpoint + deviceId + '/' + req.params.func).form({
+        access_token: accessToken,
+        args: req.params.value
+    });
+
+    res.json({
+        func: req.params.func,
+        value: req.params.value
+    });
+});
+
+
 
 /* logs */
 app.get('/log', function (req, res) {
     res.render('logs.html');
 });
+
 
 /* start server */
 console.log('node boat listening on port %d, cap\'n', port);
